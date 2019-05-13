7a708a7de16d038383cb02ce7a3cb2c53825c2d1
cacevedo <cacevedo@chequemotiva.com>
2019-05-13 13:37:14 +0200

    navbar

--- src/Key/AdminBundle/Controller/DefaultController.php
+++ src/Key/AdminBundle/Controller/DefaultController.php
@@ -10,4 +10,13 @@ class DefaultController extends Controller
     {
         return $this->render('KeyAdminBundle:Default:index.html.twig');
     }
+    public function usersAction()
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+
+        dump($users[0]->getPasswordRequestedAt());
+
+        return $this->render('KeyAdminBundle:Default:users.html.twig', array('users' => $users));
+    }
 }
